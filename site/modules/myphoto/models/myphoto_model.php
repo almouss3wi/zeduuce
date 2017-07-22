341ba6a7c24aa91776fd62b06b83e8e7abb68edd
Trung <nttrung211@gmail.com>
2017-07-22 13:28:45 +0700

    fixing the myphoto module

--- site/modules/myphoto/models/myphoto_model.php
+++ site/modules/myphoto/models/myphoto_model.php
@@ -3,10 +3,14 @@ class Myphoto_Model extends MX_Controller {
     public function __construct() {
         parent::__construct(); 
     }
-    function getPhoto($user=NULL){
+    function getPhoto($userId = NULL, $type = 1, $avatar = ''){
         $this->db->select('*')->from('user_image');
-        if($user){
-            $this->db->where("userID",$user);
+        if($userId){
+            $this->db->where("userID",$userId);
+        }
+        $this->db->where("type",$type);
+        if($avatar != ''){
+            $this->db->where('image != ',$avatar);
         }
         $query = $this->db->get()->result();
         return $query;
